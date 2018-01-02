3b82fc1e086bfc498d141aa896f54314b060d981
sklaw <271462243@qq.com>
2018-01-03 07:47:17 +0800

    add some doc

--- src/learning_rosdoc_simple_subscriber/listener.cpp
+++ src/learning_rosdoc_simple_subscriber/listener.cpp
@@ -1,8 +1,24 @@
+/** \file
+ *  \brief Implement a roscpp node that subscribes chatter topic.
+ *
+ *  The purpose of this file is for learning how to document in
+ *  cpp using doxygen, so the implementation is extremely simple.
+*/
+
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include "learning_rosdoc/MyString.h"
 
-void chatterCallback(const learning_rosdoc::MyString::ConstPtr& msg)
+/** \breif A callback function for chatter topic.
+ *
+ *  A Callback function that prints the content of
+ *  learning_rosdoc::MyString received via chatter
+ *  topic. The printing is implemented by ROS_INFO
+ *  instead of printf or cout.
+*/
+void chatterCallback(
+    const learning_rosdoc::MyString::ConstPtr& msg /**< [in] received message instance. */
+)
 {
   ROS_INFO("I heard: [%s]", msg->std_str.data.c_str());
 }
