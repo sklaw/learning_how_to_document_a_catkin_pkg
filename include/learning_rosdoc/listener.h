c2ffb7bda11cb491132b2b748026369c4fc0d774
sklaw <sk.law.lsq@gmail.com>
2018-01-08 21:22:32 +0800

    add one more function in cpp code

--- include/learning_rosdoc/listener.h
+++ include/learning_rosdoc/listener.h
@@ -1,7 +1,7 @@
 /** \file
  *  \brief Header file for listener.cpp
  *
- *  Contains declaration of my_ns namespace. Include-guarded of cource.
+ *  Contains declaration of my_cpp_ns namespace. Include-guarded of cource.
 */
 
 #ifndef LISTENER_H
@@ -12,14 +12,23 @@
  *
  *  That's all. No more detail.
 */
-namespace my_ns {
+namespace my_cpp_ns {
     /** 
      *  \brief A callback function for chatter topic that prints the
-     *    content of received message. 
      *  \param[in] msg received message instance.
      *  \return void 
     */
     void chatterCallback(const learning_rosdoc::MyString::ConstPtr& msg);
+
+
+    /// @brief This method adds two integers.
+    /// @param a First integer to add.
+    /// @param b Second integer to add.
+    /// @return The sum of both parameters.
+    int add(int a, int b);
 }
+
+
+
 
 #endif
--- src/learning_rosdoc_simple_subscriber/listener.cpp
+++ src/learning_rosdoc_simple_subscriber/listener.cpp
@@ -13,9 +13,18 @@
 /** 
  *  NOTE: The printing is implemented by ROS_INFO instead of printf or cout.
 */
-void my_ns::chatterCallback(const learning_rosdoc::MyString::ConstPtr& msg) {
+void my_cpp_ns::chatterCallback(const learning_rosdoc::MyString::ConstPtr& msg) {
   ROS_INFO("I heard: [%s]", msg->std_str.data.c_str());
 }
+
+/// This method uses a little-known variant of integer addition known as
+/// Sophocles' Scissors. It optimises the function's performance on many
+/// platforms that we may or may not choose to target in the future.
+/// @TODO make sure I implemented the algorithm correctly with some unit tests.
+int my_cpp_ns::add(int a, int b) {
+  return b + a;
+}
+
 
 int main(int argc, char **argv)
 {
@@ -23,7 +32,7 @@ int main(int argc, char **argv)
 
   ros::NodeHandle n;
 
-  ros::Subscriber sub = n.subscribe("chatter", 1000, my_ns::chatterCallback);
+  ros::Subscriber sub = n.subscribe("chatter", 1000, my_cpp_ns::chatterCallback);
 
   ros::spin();
 
